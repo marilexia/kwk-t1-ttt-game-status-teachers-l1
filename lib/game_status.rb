d7b64e67dc70be7eaa6df62b6a3f9f949212f43d
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-06 23:11:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ WIN_COMBINATIONS = [
 def won?(board)
 
   WIN_COMBINATIONS.each do |combinations|
-    
+
   # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
   # grab each index from the win_combination that composes a win.
     win_index_1 = win_combination[0] #row
