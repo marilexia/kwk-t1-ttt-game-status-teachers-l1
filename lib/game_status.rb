e9420160b91d76ff85fe5e77a2454d307981f9a8
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-06 23:46:00 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -33,7 +33,7 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination # return the win_combination indexes that won.
-    elsif position_1 == "O" && position_2 == "O" && position_3 =="O"
+    if position_1 == "O" && position_2 == "O" && position_3 =="O"
       return win_combination
     else
       return false
